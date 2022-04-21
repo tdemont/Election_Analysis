6d2fefc5e0e8c26661b7268db31253ef907c385d
tdemont <tdemont@gmail.com>
2022-04-20 17:36:27 -0700

    Add files via upload

---
+++ PyPoll.py
@@ -0,0 +1,25 @@
+#The data we need to retrieve
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote
+
+#file_to_load = 'Resources\election_results.csv'
+
+#with open (file_to_load) as election_data:
+#    print(election_data)
+
+import csv
+import os
+
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+with open (file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    headers = next(file_reader)
+    
+    print(headers)
