7e22f315e1a71f813bd366a8b63cad4ce022f077
tdemont <tdemont@gmail.com>
2022-04-20 18:11:09 -0700

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -17,9 +17,31 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+total_votes = 0
+
+candidate_options = []
+
+candidate_votes = {}
+
 with open (file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
     headers = next(file_reader)
     
-    print(headers)
+    for row in file_reader:
+        total_votes += 1
+
+        candidate_name = row[2]
+
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
+for candidate_name in candidate_votes:
+    votes = candidate_votes[candidate_name]
+    vote_percentage = float(votes)/float(total_votes)*100
+    print(f'{candidate_name}: received {vote_percentage:.1f}% of the vote.')
+#print(total_votes)
+#print(candidate_options)
+#print(candidate_votes)
+#print(candidate_name)
