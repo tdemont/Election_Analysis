2964fe488b84d6848260e024645d591870d6d5ca
tdemont <tdemont@gmail.com>
2022-04-20 18:32:50 -0700

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -23,6 +23,12 @@ candidate_options = []
 
 candidate_votes = {}
 
+winning_candidate = ""
+
+winning_count = 0
+
+winning_percentage = 0
+
 with open (file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
@@ -40,7 +46,28 @@ with open (file_to_load) as election_data:
 for candidate_name in candidate_votes:
     votes = candidate_votes[candidate_name]
     vote_percentage = float(votes)/float(total_votes)*100
-    print(f'{candidate_name}: received {vote_percentage:.1f}% of the vote.')
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        winning_count = votes
+        winning_percentage = vote_percentage
+        winning_candidate = candidate_name
+    print(f'{candidate_name}:{vote_percentage:.1f}% ({votes:,})\n')
+
+winning_candidate_summary = (
+    f"---------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"---------------\n")
+print(winning_candidate_summary)
+
+
+
+
+
+
+
+
+
 #print(total_votes)
 #print(candidate_options)
 #print(candidate_votes)
